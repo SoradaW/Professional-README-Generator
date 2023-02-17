fd0d354f6ec61f0de184c1f119c86cd36f7ebcc4
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-02-17 19:24:01 +0000

    npm install inquirer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require('path');
+const util = require('util');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
